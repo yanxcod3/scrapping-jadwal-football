967146c1fac93e9c474e3bcc601dffaab6a17dea
Yanz <riyanpratama20044@gmail.com>
2025-03-01 11:06:55 +0700

    Setup to vercel

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, Response
+from flask_cors import CORS
 import json
 from scraper import scrape_data
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route("/api", methods=["GET"])
 def get_scraped_data():
@@ -12,4 +14,4 @@ def get_scraped_data():
     return response
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    app.run(host='0.0.0.0', port=5000)
